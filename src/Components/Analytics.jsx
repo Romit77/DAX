b8f5188eae16a24103a20ad489ab953ac8d939d4
Romit  Dey <romit77dey@gmail.com>
2024-02-22 14:54:44 +0530

    fixed issues

--- src/Components/Analytics.jsx
+++ src/Components/Analytics.jsx
@@ -37,7 +37,7 @@ const Analytics = () => {
               help you make sense of your data. Start harnessing the potential
               of data today.
             </motion.p>
-            <motion.button className="rounded-2xl border-2 border-dashed border-black bg-[#00df98] px-1 py-3 font-semibold  text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none w-[150px] my-2 md:mx-[0px] mt-5">
+            <motion.button className="rounded-2xl border-2  border-black bg-[#00df98] px-1 py-3 font-semibold  text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none w-[150px] my-2 md:mx-[0px] mt-5">
               Get started
             </motion.button>
           </div>
