452fd2569c8dddc4ed2b6f5f989ce9dc64912515
Romit  Dey <romit77dey@gmail.com>
2024-03-03 19:26:56 +0530

    fixed favicon

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -87,7 +87,7 @@ const Main = () => {
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
         <motion.p
           initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1 }}
         >
           Growing your business is hard. Financing it shouldn<span>&#39;</span>t
@@ -96,7 +96,7 @@ const Main = () => {
         <motion.h1
           className="bg-gradient-to-r from-blue-600 via-green-500  text-transparent bg-clip-text md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 mb-3 "
           initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1 }}
         >
           Grow With Data
@@ -105,7 +105,7 @@ const Main = () => {
           <motion.p
             className="md:text-5xl sm:text-4xl text-xl font-bold text-slate-500 mb-4"
             initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 1 }}
           >
             Fast , Flexible Financing for{" "}
@@ -134,7 +134,7 @@ const Main = () => {
         <motion.div
           className="w-[200px] rounded-md font-bold my-6 mx-auto p-3 text-black "
           initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1.5 }}
           drag
           dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
